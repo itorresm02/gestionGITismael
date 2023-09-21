0bc05d38eb831f4d15173e272aee1d2d59c359bb
DESKTOP-CJ5CJHV\daw1 <ismaeltorresmede@gmail.com>
2023-09-21 16:45:45 +0200

    tabla del 12

--- proyecto git/src/App.java
+++ proyecto git/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Buenas Tardes");
+        
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
     }
 }
